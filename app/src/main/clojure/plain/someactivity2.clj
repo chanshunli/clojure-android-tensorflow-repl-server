40c3c8073999c77a7cabeb3b21cc7cc281869c59
Steve Chan <chanshunli@gmail.com>
2018-04-14 02:04:49 +0800

    refactoring

--- app/src/main/clojure/plain/someactivity2.clj
+++ app/src/main/clojure/plain/someactivity2.clj
@@ -1,20 +1,19 @@
 (ns plain.someactivity2
-  (:require [clojure.tools.nrepl.server :as repl])
+  (:require [clojure.tools.nrepl.server :as repl]
+            [clojure.core.async :as a]
+            [org.httpkit.client :as http])
   (:import
-    (android.support.v7.app AppCompatActivity)
-    (android.util Log)
-    (com.example.ndksample.myapplication.R$id)
-    (com.example.ndksample.myapplication.R$layout)
-    (android.os Handler)
-    (org.tensorflow.contrib.android TensorFlowInferenceInterface))
-  (:require [org.httpkit.client :as http])
-
+   (android.support.v7.app AppCompatActivity)
+   (android.util Log)
+   (com.example.ndksample.myapplication.R$id)
+   (com.example.ndksample.myapplication.R$layout)
+   (android.os Handler)
+   (org.tensorflow.contrib.android TensorFlowInferenceInterface))
   (:gen-class
-    :name "plain.someactivity2.MyActivity"
-    :exposes-methods {onCreate superOnCreate}
-    :extends android.support.v7.app.AppCompatActivity
-    :prefix "some-")
-  )
+   :name "plain.someactivity2.MyActivity"
+   :exposes-methods {onCreate superOnCreate}
+   :extends android.support.v7.app.AppCompatActivity
+   :prefix "some-"))
 
 (defn fetch [url]
   (http/get url))
@@ -39,5 +38,5 @@
   (let [tv (.findViewById this com.example.ndksample.myapplication.R$id/text)
         handler (Handler.)]
     (.start (Thread. (fn []
-                       (let [data (:body @(fetch "http://www.yahoo.co.jp"))]
+                       (let [data (:body @(fetch "https://clojure.org"))]
                          (.post handler #(.setText tv data))))))))
